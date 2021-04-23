4df6e5dda743f18de7910b2d8f6044495be0295b
Jan Schneider <j.schneider@uib.de>
2021-04-23 14:15:19 +0200

    [fix] Fix arglist

--- ocainstallationhelper/__main__.py
+++ ocainstallationhelper/__main__.py
@@ -111,8 +111,8 @@ class InstallationHelper:  # pylint: disable=too-many-instance-attributes
 			)
 		] #,"/PARAMETER INSTALL:CREATE_CLIENT:REBOOT"
 
-		arg_list = ",".join(arg_list)
-		ps_script = f'"Start-Process -FilePath \\"{opsi_script}\\" -ArgumentList {arg_list} -Wait"'
+		arg_list = ",".join([f'"{arg}"' for arg in arg_list])
+		ps_script = f'Start-Process -FilePath "{opsi_script}" -ArgumentList {arg_list} -Wait"'
 		logger.debug(ps_script)
 		ps_script_file = os.path.join(self.base_dir, "setup.ps1")
 		with codecs.open(ps_script_file, "w", "windows-1252") as file:
@@ -358,6 +358,8 @@ class InstallationHelper:  # pylint: disable=too-many-instance-attributes
 			if event == "install":
 				try:
 					self.install()
+					for _num in range(5):
+						time.sleep(1)
 					return
 				except BackendAuthenticationError as err:
 					self.show_message("Authentication error, wrong username or password", "error")

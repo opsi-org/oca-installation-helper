da28727b8ad7de5c9c90cf2d4fa1a9e027dbb523
doerrer <n.doerrer@uib.de>
2021-11-17 15:28:53 +0100

    [fix,pub] fixed subprocess call for linux systems with outdated libraries

--- ocainstallationhelper/__main__.py
+++ ocainstallationhelper/__main__.py
@@ -34,6 +34,28 @@ from ocainstallationhelper.console import ConsoleDialog
 from ocainstallationhelper.gui import GUIDialog
 
 
+def monkeypatch_subprocess_for_frozen():
+	from subprocess import Popen as Popen_orig
+	class Popen_patched(Popen_orig):
+		def __init__(self, *args, **kwargs):
+			if kwargs.get("env") is None:
+				kwargs["env"] = os.environ.copy()
+			lp_orig = kwargs["env"].get("LD_LIBRARY_PATH_ORIG")
+			if lp_orig is not None:
+				# Restore the original, unmodified value
+				kwargs["env"]["LD_LIBRARY_PATH"] = lp_orig
+			else:
+				# This happens when LD_LIBRARY_PATH was not set.
+				# Remove the env var as a last resort
+				kwargs["env"].pop("LD_LIBRARY_PATH", None)
+
+			super().__init__(*args, **kwargs)
+
+	subprocess.Popen = Popen_patched
+
+
+monkeypatch_subprocess_for_frozen()
+
 KEY = "ahmaiweepheeVee5Eibieshai4tei7nohhochudae7show0phahmujai9ahk6eif"
 
 def encode_password(cleartext):

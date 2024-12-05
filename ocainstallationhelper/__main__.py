b6b28672769e7b4b67719cda735c849954995c26
doerrer <n.doerrer@uib.de>
2024-12-05 08:56:35 +0100

    [chg] debug information on opsi-script skin copy

--- ocainstallationhelper/__main__.py
+++ ocainstallationhelper/__main__.py
@@ -85,9 +85,14 @@ class InstallationHelper:
 		await self.show_message(f"Installation files succesfully copied to '{self.tmp_dir}'", "success")
 		self.config.opsi_script = self.tmp_dir / "opsi-script" / self.config.opsi_script_path
 		try:
+			logger.debug(
+				"Copying opsi-script skin files from %s to %s",
+				self.tmp_dir / "opsi-script" / "common" / "skin",
+				self.config.opsi_script.parent,
+			)
 			shutil.copytree(self.tmp_dir / "opsi-script" / "common" / "skin", self.config.opsi_script.parent)
-		except Exception:
-			logger.warning("Failed to copy opsi-script skin files")
+		except Exception as error:
+			logger.warning("Failed to copy opsi-script skin files: %s", error)
 		make_executable(self.config.opsi_script)
 		return self.tmp_dir / self.config.oca_package
 

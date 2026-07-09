898b149c1c2eabf031bec96c7ae632a524cd74e2
Franziska Mueller <f.mueller@uib.de>
2026-07-09 16:29:43 +0200

    [chg] improments requested in MR 43

--- ocainstallationhelper/console.py
+++ ocainstallationhelper/console.py
@@ -88,7 +88,7 @@ class ConsoleDialog(BaseDialog, App):
 			self.inputs["client_id"],
 			PaddedLabel("Opsi Service url"),
 			self.inputs["service_address"],
-			PaddedLabel("Username (Config Server)"),
+			PaddedLabel("Username (on Config Server)"),
 			self.inputs["service_username"],
 			PaddedLabel("Password"),
 			self.inputs["service_password"],

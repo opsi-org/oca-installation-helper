55a372583464c4d5f03d28658849074ae6630d75
Jan Schneider <j.schneider@uib.de>
2026-08-05 13:29:38 +0200

    [chg,pub] Build 64bit version of oca-installation-helper for Windows

--- ocainstallationhelper/__init__.py
+++ ocainstallationhelper/__init__.py
@@ -19,7 +19,7 @@ from opsi.logging import get_logger
 if TYPE_CHECKING:
 	from ocainstallationhelper.__main__ import InstallationHelper
 
-__version__ = "4.3.3.4"
+__version__ = "4.3.8.0"
 
 
 KEY = "ahmaiweepheeVee5Eibieshai4tei7nohhochudae7show0phahmujai9ahk6eif"
